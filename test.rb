5d0ce4e2be3265179cafe6c8616c7846fe15d210
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2012-06-22 12:06:55 +0200

    Update Outputs settings
    - Remove H.264 High profile outputps
    - Add device_profile params
    - Reduce 3GP quality to 2 (so it's smaller)
    - Limits thumbnail to 5 per size

--- test.rb
+++ test.rb
@@ -9,21 +9,31 @@ class Video
     end
     @user_id = 1
   end
-  
+
   # Workaround for ERB binding
   def get_binding
     binding()
   end
 end
 
-video = Video.new(
-  # token:   'zencodertesting_test',
-  # path:    'http://s3.amazonaws.com/zencodertesting/test.mov',
-  token:   'sublime_720',
-  path:    'http://media.jilion.com/videos/demo/midnight_sun_sv1_720p.mp4',
-)
+experiment = 'normal'
 
-response = ZencoderWrapper.create_job(video)
-puts "Status code: #{response.code}"
-puts "ID: #{response.body['id']}"
-puts "errors: #{response.body['errors']}"
+videos = []
+videos << Video.new(
+  token: "#{experiment}-sublime_720",
+  path:  'http://media.jilion.com/videos/demo/midnight_sun_sv1_720p.mp4')
+videos << Video.new(
+  token: "#{experiment}-zencodertesting_test",
+  path:  'http://s3.amazonaws.com/zencodertesting/test.mov')
+videos << Video.new(
+  token: "#{experiment}-steve_d8",
+  path:  'http://m.wsj.net/video/20100602/060110atdjobstablet/060110atdjobstablet_1500k.mp4')
+
+videos.each do |video|
+  puts "--------"
+  puts "Video: #{video.token}"
+  response = ZencoderWrapper.create_job(video)
+  puts "Status code: #{response.code}"
+  puts "ID: #{response.body['id']}"
+  puts "errors: #{response.body['errors']}"
+end
